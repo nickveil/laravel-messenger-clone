4260c4c5909c82a0d955c38a132550313ba2c80d
nickveil <31191433+nickveil@users.noreply.github.com>
2020-09-07 09:40:43 -0500

    setup vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,8 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('chat-form', require('./components/ChatForm.vue').default);
+Vue.component('chat-messages', require('./components/ChatMessages.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +31,28 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+
+    data: {
+        messages: []
+    },
+
+    created() {
+        this.fetchMessages();
+    },
+
+    methods: {
+        fetchMessages() {
+            axios.get('/messages').then(response => {
+                this.messages = response.data;
+            });
+        },
+
+        addMessages() {
+            this.messages.push(message);
+
+            axios.post('/messages', message).then(response => {
+                console.log(response.data);
+            })
+        }
+    }
 });
--- resources/js/components/ChatMessages.vue
+++ resources/js/components/ChatMessages.vue
@@ -1,14 +1,28 @@
 <template>
-    <blockquote class="blockquote text-center">
+    <!-- <blockquote class="blockquote text-center" v-for="message in messages">
         <p class="mb-0">{{message.message}}</p>
         <footer class="blockquote-footer">{{message.user.name}}</footer>
-    </blockquote>
+    </blockquote> -->
+    <ul class="chat">
+        <li class="left clearfix" v-for="message in messages">
+            <div class="chat-body clearfix">
+                <div class="header">
+                    <strong class="primary-font">
+                        {{ message.user.name }}
+                    </strong>
+                </div>
+                <p>
+                    {{ message.message }}
+                </p>
+            </div>
+        </li>
+    </ul>
 </template>
 
 <script>
-export default {
-    props: ['message']
-};
+  export default {
+    props: ['messages']
+  };
 </script>
 
 <style>
