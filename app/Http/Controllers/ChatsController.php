a9d7985c4448028d1762d066dd2249263996addd
nickveil <31191433+nickveil@users.noreply.github.com>
2020-09-07 09:53:59 -0500

    created messagesent event and updated conteroller and route

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -44,6 +44,8 @@ class ChatsController extends Controller
         $message = $user->messages()-create([
             'message' => $request->input('message')
         ]);
+
+        broadcast(new MessageSent($user, $message))->toOthers();
         
         return ['status' => 'Message Sent!'];
     }
